84cc1b3db6c3b685711eda7be573f9f1fc45d2f1
Jorge Clavijo <jym272@gmail.com>
2023-04-30 21:09:46 -0300

    feat: logging getCurrentUser

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -5,11 +5,15 @@ import { JwtPayloadCustom } from '@src/types';
 export const fetcher = (url: string) => fetch(url).then(res => res.json());
 
 export const getCurrentUser = async ({ req }: { req: NextApiRequest | undefined }) => {
+    // eslint-disable-next-line no-console
+    const log = console.log;
+
     const isServerSide = !!req;
     let url = '/api/users/current-user';
     const headers = {};
     if (isServerSide) {
         if (!req.headers.cookie) {
+            log("No cookie, don't fetch the current user");
             return {
                 fallback: {
                     '/api/users/current-user': {
@@ -31,7 +35,9 @@ export const getCurrentUser = async ({ req }: { req: NextApiRequest | undefined 
         method: 'GET',
         headers
     });
+    const isServerSideMsg = isServerSide ? 'server side' : 'client side';
     if (!res.ok) {
+        log(`No response from the server, fetch the current user from ${isServerSideMsg}}`);
         return {
             fallback: {
                 '/api/users/current-user': {
@@ -41,6 +47,7 @@ export const getCurrentUser = async ({ req }: { req: NextApiRequest | undefined 
         };
     }
     const data = (await res.json()) as { currentUser: null | JwtPayloadCustom };
+    log(`Fetch the current user from ${isServerSideMsg}`);
     return {
         fallback: {
             '/api/users/current-user': data
