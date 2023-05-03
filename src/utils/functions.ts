01bb3ba2360bd9e93a3f9988ab6feb23fcd7939d
Jorge Clavijo <jym272@gmail.com>
2023-05-03 14:03:10 -0300

    feat: ingress method

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -24,18 +24,38 @@ export const getCurrentUser = async ({ req }: { req: NextApiRequest | undefined 
         }
         const namespace = getEnvOrFail('INGRESS_NS');
         const service = getEnvOrFail('INGRESS_SVC');
-        const ingressUrl = `http://${service}.${namespace}.svc.cluster.local/api/users/current-user`;
+        const method = getEnvOrFail('INGRESS_METHOD'); // http or https
+        const ingressUrl = `${method}://${service}.${namespace}.svc.cluster.local/api/users/current-user`;
         // the controller endpoint need the cookies, the ingress need the Host header
         // Object.assign(headers, { cookie: req.headers.cookie, Host: 'ticketing.dev' });
         // proxy the headers from the web browser to the ingress
+        //  nc -vz aws-load-balancer-webhook-service.kube-system.svc.cluster.local 80
+        // https://aws-load-balancer-webhook-service.kube-system.svc.cluster.local/api/users/current-user , add Header Host: ticketing.dev and send a get method with curl
+        // curl -H "Host: dev.jym272.foundation" https://aws-load-balancer-webhook-service.kube-system.svc.cluster.local/api/users/current-user
+        //
         Object.assign(headers, req.headers);
         url = ingressUrl;
     }
-    const res = await fetch(url, {
-        method: 'GET',
-        headers
-    });
+
     const isServerSideMsg = isServerSide ? 'server side' : 'client side';
+    let res: Response;
+
+    try {
+        res = await fetch(url, {
+            method: 'GET',
+            headers
+        });
+    } catch (err) {
+        log(`No response from the server, fetch the current user from ${isServerSideMsg}}`);
+        return {
+            fallback: {
+                '/api/users/current-user': {
+                    currentUser: null
+                }
+            }
+        };
+    }
+
     if (!res.ok) {
         log(`No response from the server, fetch the current user from ${isServerSideMsg}}`);
         return {
